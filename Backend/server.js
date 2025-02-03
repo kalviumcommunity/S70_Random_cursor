61d0cc7ffaeb0a52e63f115447119704b46c9b98
Prahalad Singh Gaur <singhgaurprahalad@gmail.com>
2025-02-03 15:14:37 +0530

    Assignment | ASAP Project - MongoDB client

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,38 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
+let dbConnectionStatus = "Disconnected";
 
-app.get('/ping',(req,res)=>{
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        dbConnectionStatus = "Connected to MongoDB";
+        console.log(dbConnectionStatus);
+    })
+    .catch((error) => {
+        dbConnectionStatus = "MongoDB Connection Failed";
+        console.error(error);
+    });
+
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: dbConnectionStatus
+    });
+});
+
+
+app.get('/ping', (req, res) => {
     res.status(200).send({
         message: "API is working now"
-    })
-})                                                                                        
+    });
+});
 
-app.listen(PORT,()=>{console.log(`The server is running at http://localhost:${PORT}`)});
+app.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}`);
+});
